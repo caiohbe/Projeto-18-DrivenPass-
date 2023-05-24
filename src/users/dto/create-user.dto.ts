afb1f6e47b7cc08e18e0d3a823a5e2ea8592b7d8
unknown <caiohbe@gmail.com>
2023-05-24 14:53:34 -0300

    fix: strong password configed

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -7,6 +7,12 @@ export class CreateUserDto {
   @IsEmail()
   email: string;
 
-  @IsStrongPassword() //FIXME
+  @IsStrongPassword({
+    minLength: 10,
+    minNumbers: 1,
+    minLowercase: 1,
+    minUppercase: 1,
+    minSymbols: 1,
+  })
   password: string;
 }
